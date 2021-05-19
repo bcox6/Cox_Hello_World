bc497c0c47bec322d205b7aa71e62c47f39e9a01
itpipesbrian <38361062+itpipesbrian@users.noreply.github.com>
2021-05-19 13:57:50 -0600

    Add project files.

---
+++ Cox_Hello_World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cox_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
